312057bfdd087435d419f9b20f8ed159d72ab9e2
tarmin68 <38571096+tarmin68@users.noreply.github.com>
2019-02-17 18:16:58 -0500

    Health Step 4

--- src/Test.java
+++ src/Test.java
@@ -3,16 +3,16 @@ public class Test {
 	public static void main(String[] args) {
 //		testPolicyRandomDataGeneration("HealthCareDatset2/healthCareRules", "HealthCareDatset2/Attribute.txt", "HealthCareDatset2/AttributeValue.txt");
 		
-		testGenerateFN_FPRecords("HealthCareDatset2/step3ExtractedRuleFPRefine", "HealthCareDatset2/healthCareRulesPermitted.txt", "HealthCareDatset2/healthCareRulesDenied.txt", "HealthCareDatset2/Attribute.txt", "HealthCareDatset2/AttributeValue.txt");
+		testGenerateFN_FPRecords("HealthCareDatset2/step4ExtractedRuleAfterFNRefine", "HealthCareDatset2/healthCareRulesPermitted.txt", "HealthCareDatset2/healthCareRulesDenied.txt", "HealthCareDatset2/Attribute.txt", "HealthCareDatset2/AttributeValue.txt");
 		
 //		testGenerateFN_FPRecords("UniversityDataset2/step2ExtractedRulesAfterFNRefine", "UniversityDataset2/DataSet2RulesPermitted.txt", "UniversityDataset2/DataSet2RulesDenied.txt");
 		
-//		testGenerateFN_FPRecords("UniversityDataset3/step3ExtractedRulesAfterFNRefine", "UniversityDataset3/permittedtest.txt", "UniversityDataset3/deniedtest.txt", "UniversityDataset3/Attribute.txt", "UniversityDataset3/AttributeValue.txt");
+//		testGenerateFN_FPRecords("UniversityDataset3/step4ExtractedRulesFPRefine", "UniversityDataset3/permittedtest.txt", "UniversityDataset3/deniedtest.txt", "UniversityDataset3/Attribute.txt", "UniversityDataset3/AttributeValue.txt");
 		
 //		refineBasedonFNs("UniversityDataset3/step2ExtractedRulesAfterFNRefine", "UniversityDataset3/step3FNExtractedRules", "UniversityDataset3/permittedtest.txt" , "UniversityDataset3/deniedtest.txt", "UniversityDataset3/Attribute.txt", "UniversityDataset3/AttributeValue.txt");
 //		refineBasedonFNs("UniversityDataset2/step1ExtractedRules", "UniversityDataset2/step2FNExtractedRules", "UniversityDataset2/DataSet2RulesPermitted.txt" , "UniversityDataset2/DataSet2RulesDenied.txt");
 	
-//		refineBasedonFNs("HealthCareDatset2/newstep1ExtractedRules", "HealthCareDatset2/step2FNExtractedRules", "HealthCareDatset2/healthCareRulesPermitted.txt" , "HealthCareDatset2/healthCareRulesDenied.txt", "HealthCareDatset2/Attribute.txt", "HealthCareDatset2/AttributeValue.txt");
+//		refineBasedonFNs("HealthCareDatset2/step3ExtractedRuleFPRefine", "HealthCareDatset2/step4FNExtractedRules", "HealthCareDatset2/healthCareRulesPermitted.txt" , "HealthCareDatset2/healthCareRulesDenied.txt", "HealthCareDatset2/Attribute.txt", "HealthCareDatset2/AttributeValue.txt");
 		
 //		refineBasedonFPs("UniversityDataset2/step2ExtractedRulesAfterFNRefine", "UniversityDataset2/step3FPExtractedRules", "UniversityDataset2/DataSet2RulesPermitted.txt" , "UniversityDataset2/DataSet2RulesDenied.txt");
 		
